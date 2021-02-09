5bf9b0e9b477086b3d3a1c1407e836fd1108bda0
Paul_D_Edward <edward2000ed@gmail.com>
2021-02-09 19:23:26 +0530

    Create pattern_6.c

---
+++ pattern_6.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int number = 5,count = 0;
+
+    for(int i = number; i > 0; i--)
+    {
+        for(int j = number; j > 0; j--)
+        {
+
+           if( j > ((number - i) + 1) )
+           {
+               printf("  ");
+           }
+           else
+           {
+             printf("%d ",j);
+           }
+        }
+        for(int j = 1; j < number; j++)
+        {
+
+           if( j < ((number - i) + 1) )
+           {
+               printf("%d ",j + 1);
+           }
+           else
+           {
+
+             printf("  ");
+           }
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
