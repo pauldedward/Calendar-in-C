3fdaff21357aacd0b421cc0c50cdc5cd306714a1
Paul_D_Edward <edward2000ed@gmail.com>
2021-02-09 18:49:57 +0530

    Create pattern_2.c

---
+++ pattern_2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int number = 5;
+
+    for(int i = 0; i < number; i++)
+    {
+        for(int j = 0; j <= i; j++)
+        {
+            printf("%d ",j+1);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
